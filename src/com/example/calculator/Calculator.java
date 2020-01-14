27bf2ccad913a07f1b12af2ad183f152806233cf
igotflaggedlol <tears@waifu.club>
2020-01-14 03:14:52 +0100

    Third commit, added a loop for the app. (#3)

--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -26,40 +26,49 @@ class calculator {
 
     // Main
     public static void main(String[] args) {
-        Scanner userInput = new Scanner(System.in); // Makes scanner for user input
+        String repeatCondition;
 
-        System.out.println("Please type in whether you want to use one of these following methods:\n(add)ition" +
-                                                                                                 "\n(sub)traction" +
-                                                                                                 "\n(div)ision" +
-                                                                                                 "\n(mul)tiplication");
+        do {
+            Scanner userInput = new Scanner(System.in); // Makes scanner for user input
 
-        String calcMethod = userInput.nextLine(); // Asks for method of calculation
+            System.out.println("Please type in whether you want to use one of these following methods:\n(add)ition" +
+                    "\n(sub)traction" +
+                    "\n(div)ision" +
+                    "\n(mul)tiplication");
 
-        System.out.println("Please enter your first and second number: ");
+            String calcMethod = userInput.nextLine(); // Asks for method of calculation
 
-        int userNum1 = userInput.nextInt(); // Asks for first number
-        int userNum2 = userInput.nextInt(); // Asks for second number
+            System.out.println("Please enter your first and second number: ");
 
-        switch (calcMethod) {
-            case "add":
-                System.out.println("Your answer is: " + addition(userNum1, userNum2)); // Shows result
-                break;
+            int userNum1 = userInput.nextInt(); // Asks for first number
+            int userNum2 = userInput.nextInt(); // Asks for second number
 
-            case "sub":
-                System.out.println("Your answer is: " + subtraction(userNum1, userNum2)); // Shows result
-                break;
+            switch (calcMethod) {
+                case "add":
+                    System.out.println("Your answer is: " + addition(userNum1, userNum2)); // Shows result
+                    break;
 
-            case "mul":
-                System.out.println("Your answer is: " + multiplication(userNum1, userNum2)); // Shows result
-                break;
+                case "sub":
+                    System.out.println("Your answer is: " + subtraction(userNum1, userNum2)); // Shows result
+                    break;
 
-            case "div":
-                System.out.println("Your answer is: " + division(userNum1, userNum2)); // Shows result
-                break;
+                case "mul":
+                    System.out.println("Your answer is: " + multiplication(userNum1, userNum2)); // Shows result
+                    break;
 
-            default:
-                System.out.println("Please enter a valid method");
-                break;
+                case "div":
+                    System.out.println("Your answer is: " + division(userNum1, userNum2)); // Shows result
+                    break;
+
+                default:
+                    System.out.println("Please enter a valid method");
+                    break;
+            }
+
+            System.out.println("Do you want to try again? (Y)/(N): ");
+            repeatCondition = userInput.next();
         }
+
+        while (repeatCondition.equals("Y"));
     }
 }
